608b76db7ea7ab42a5803df1203f089389d1f7ee
Ekansh Singh <ekanshsin232@gmail.com>
2023-07-25 11:08:18 +0530

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Stack from '@mui/material/Stack';
+import CustomizedInputBase from './components/SearchBar';
+import { Box, Divider, Typography } from '@mui/material';
+import Tags from './components/Tags';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box style={{
+      width:"100% !important",
+     
+    }} >
+       <Stack  width="100%" height="100vh"  alignItems='center' mt={2} > 
+      <Box sx={{width:"100%"}}> 
+<Typography variant='h3' gutterBottom sx={{textAlign:'center', fontWeight:'bold' }}>LOGO</Typography>
+      </Box>
+      
+<CustomizedInputBase/>
+<Tags/>
+<Divider width='100%' mt={2}  />
+      </Stack> 
+     
+    </Box>
   );
 }
 
---
+++ src/components/Tags.js
@@ -0,0 +1,22 @@
+import { Chip, Stack } from '@mui/material'
+import React from 'react'
+
+const Tags = () => {
+
+    const handleClick = (e) => {
+        console.info('You clicked the Chip.');
+        console.log(e);
+      };
+    
+
+  return (
+    <Stack direction="row" spacing={1}>
+    <Chip label="Extra Soft" onClick={(e)=>handleClick(e)}  />
+    <Chip label="Soft"  onClick={(e)=>handleClick(e)}/>
+    <Chip label="Medium" onClick={(e)=>handleClick(e)}/>
+    <Chip label="Hard"  onClick={(e)=>handleClick(e)}/>
+  </Stack>
+  )
+}
+
+export default Tags
